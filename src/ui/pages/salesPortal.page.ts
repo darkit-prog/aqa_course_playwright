bccf136ce9a7199aafcdfa9864a31b257920c6c5
darkit-prog <darya.kitaina@gmail.com>
2025-11-13 22:57:56 +0300

    make task 2

--- src/ui/pages/salesPortal.page.ts
+++ src/ui/pages/salesPortal.page.ts
@@ -1,6 +1,5 @@
 import { expect, Locator } from "@playwright/test";
 import { BasePage } from "./base.page";
-import { SALES_PORTAL_URL } from "../../config/env";
 
 export abstract class SalesPortalPage extends BasePage {
   readonly spinner = this.page.locator(".spinner-border");
@@ -10,9 +9,5 @@ export abstract class SalesPortalPage extends BasePage {
   async waitForOpened() {
     await expect(this.uniqueElement).toBeVisible();
     await expect(this.spinner).toHaveCount(0);
-  }
-
-  async open() {
-    await this.page.goto(SALES_PORTAL_URL);
   }
 }
---
+++ src/ui/pages/signIn.page.ts
@@ -0,0 +1,25 @@
+// Написать Page Object класс для страницы Sign In:
+//   - email input
+//   - password input
+//   - login button
+//   - fillCredentials method
+//   - click on login button method
+import { BasePage } from "./base.page";
+import { SALES_PORTAL_URL } from "../../config/env";
+import { ICredentials } from "../../data/types/credentials.types";
+
+export class SignInPage extends BasePage {
+    readonly emailInput = this.page.locator("#emailinput");
+    readonly passwordInput = this.page.locator("#passwordinput");
+    readonly loginButton = this.page.locator("button[type='submit']");
+
+    async open() {
+        await this.page.goto(SALES_PORTAL_URL);
+    }
+
+    async sighIn(creds: ICredentials) {
+        await this.emailInput.fill(creds.username);
+        await this.passwordInput.fill(creds.password);
+        await this.loginButton.click();
+    }
+}
